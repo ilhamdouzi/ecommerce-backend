aa418462ac8aeae6ec82125bbaf1041fb20c2c7d
ilham <douzi.ilham@gmail.com>
2025-02-24 23:28:37 +0000

    feat(cart): add CartItemRepository

---
+++ src/main/java/com/app/ecommerce_backend/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.app.ecommerce_backend.repository;
+
+import com.app.ecommerce_backend.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
