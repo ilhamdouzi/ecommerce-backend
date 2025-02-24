86dd8cb20dea75b55acc305411af9cb9f7431bc2
ilham <douzi.ilham@gmail.com>
2025-02-24 21:05:16 +0000

    feat(auth): add logs in controller layer

--- src/main/java/com/app/ecommerce_backend/controller/AuthController.java
+++ src/main/java/com/app/ecommerce_backend/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.app.ecommerce_backend.dto.AuthResponse;
 import com.app.ecommerce_backend.dto.RegisterRequest;
 import com.app.ecommerce_backend.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,19 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@Slf4j
 public class AuthController {
 
     private final AuthService authService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        log.info("Received login request for email: {}", request.email());
         AuthResponse tokens = authService.authenticate(request);
+        log.info("User authenticated successfully for email: {}", request.email());
         return ResponseEntity.ok(tokens);
     }
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        log.info("Received registration request for username: {}", request.username());
         String msg = authService.register(request);
+        log.info("User registered successfully: {}", request.username());
         return ResponseEntity.ok(msg);
     }
 }
