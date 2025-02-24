36ff41f0253327a73e562b9c9ea86d6c36c3f09f
ilham <douzi.ilham@gmail.com>
2025-02-24 20:27:53 +0000

    feat(auth): add UserDetailsService implementation

---
+++ src/main/java/com/app/ecommerce_backend/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.app.ecommerce_backend.service.impl;
+
+import com.app.ecommerce_backend.exception.ResourceNotFoundException;
+import com.app.ecommerce_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> org.springframework.security.core.userdetails.User
+                        .withUsername(user.getEmail())
+                        .password(user.getPassword())
+                        .roles(user.getRole())
+                        .build())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with email: " + email));
+
+    }
+}
